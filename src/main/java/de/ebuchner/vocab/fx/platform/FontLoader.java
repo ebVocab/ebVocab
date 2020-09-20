aa581672f816d94ca89db0e306e3d523aaf0323b
eb <eb_ontour@hotmail.com>
2020-09-20 15:47:12 +0200

    Removed PrismFontLoader hack

--- src/main/java/de/ebuchner/vocab/fx/platform/FontLoader.java
+++ src/main/java/de/ebuchner/vocab/fx/platform/FontLoader.java
@@ -1,19 +1,16 @@
 package de.ebuchner.vocab.fx.platform;
 
-import com.sun.javafx.font.PrismFontLoader;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 
 public class FontLoader {
-    private static PrismFontLoader nativeFontLoader = new PrismFontLoader();
-
     private FontLoader() {
 
     }
 
     public static Font font(String name, FontWeight fontWeight, FontPosture fontPosture, float size) {
-        return nativeFontLoader.font(
+        return Font.font(
                 name,
                 fontWeight,
                 fontPosture,
